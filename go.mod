f97bfb3ba077e6a96f63947b943dc6be66e8b51e
Xander Warszawski <xander@xdoubleu.com>
2023-11-29 22:57:49 +0100

    Added basic configuration and middleware (#17)

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/XDoubleU/essentia
+
+go 1.21.4
+
+require (
+	github.com/goddtriffin/helmet v1.0.2
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/rs/cors v1.10.1
+	golang.org/x/time v0.5.0
+)
---
+++ responseWriter.go
@@ -0,0 +1,30 @@
+package essentia
+
+import (
+	"bufio"
+	"errors"
+	"net"
+	"net/http"
+)
+
+type ResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{w, http.StatusOK}
+}
+
+func (w *ResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *ResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	h, ok := w.ResponseWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, errors.New("hijack not supported")
+	}
+	return h.Hijack()
+}
