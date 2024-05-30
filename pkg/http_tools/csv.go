580ec33fe208e6d1860787883088ec290636e93f
Xander Warszawski <xander@xdoubleu.com>
2024-05-30 20:46:00 +0200

    WIP

---
+++ pkg/http_tools/csv.go
@@ -0,0 +1,17 @@
+package http_tools
+
+import (
+	"encoding/csv"
+	"fmt"
+	"net/http"
+)
+
+func WriteCSV(w http.ResponseWriter, filename string, data [][]string) error {
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().
+		Set("Content-Disposition", fmt.Sprintf("attachment;filename=%s.csv", filename))
+
+	csvWriter := csv.NewWriter(w)
+
+	return csvWriter.WriteAll(data)
+}
