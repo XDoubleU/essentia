f1232d53c43be505f6d78e04e37dcf0be47fbf91
Xander Warszawski <xander@xdoubleu.com>
2024-11-05 20:33:16 +0100

    Actually ignore WS errors

--- pkg/communication/ws/errors.go
+++ pkg/communication/ws/errors.go
@@ -24,10 +24,6 @@ func ErrorResponse(
 	errorDto := errortools.NewErrorDto(status, message)
 	err := wsjson.Write(ctx, conn, errorDto)
 	if err != nil {
-		if canIgnoreError(err) {
-			return
-		}
-
 		contexttools.Logger(ctx).
 			ErrorContext(ctx, "failed to write JSON", logging.ErrAttr(err))
 	}
@@ -36,6 +32,10 @@ func ErrorResponse(
 // ServerErrorResponse is used to handle
 // internal server errors that occurred on a WebSocket.
 func ServerErrorResponse(ctx context.Context, conn *websocket.Conn, err error) {
+	if isCloseError(err) {
+		return
+	}
+
 	contexttools.Logger(ctx).
 		ErrorContext(ctx, "server error occurred", logging.ErrAttr(err))
 
@@ -50,7 +50,7 @@ func ServerErrorResponse(ctx context.Context, conn *websocket.Conn, err error) {
 // UpgradeErrorResponse is used to handle an error that
 // occurred during the upgrade towards a WebSocket.
 func UpgradeErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	if !canIgnoreError(err) {
+	if !isWSProtocolViolation(err) {
 		contexttools.Logger(r.Context()).
 			ErrorContext(r.Context(), "WS upgrade error occurred", logging.ErrAttr(err))
 	}
@@ -74,10 +74,13 @@ func ForbiddenResponse(ctx context.Context, conn *websocket.Conn) {
 	ErrorResponse(ctx, conn, http.StatusForbidden, errortools.MessageForbidden)
 }
 
-func canIgnoreError(err error) bool {
-	// EOF, close errors and WS Protocol Violations can be ignored
+func isWSProtocolViolation(err error) bool {
+	return strings.Contains(err.Error(), "WebSocket protocol violation")
+}
+
+func isCloseError(err error) bool {
+	// EOF, close errors
 	var closeError websocket.CloseError
 	return errors.Is(err, io.EOF) ||
-		errors.As(err, &closeError) ||
-		strings.Contains(err.Error(), "WebSocket protocol violation")
+		errors.As(err, &closeError)
 }
